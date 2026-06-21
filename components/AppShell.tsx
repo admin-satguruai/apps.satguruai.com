5b6d4ccd469521903b6adb830de33932d7af8685
admin-satguruai <admin@satguruai.com>
2026-06-21 14:01:56 +0530

    Fix shell spacing for collapsed enterprise sidebar

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -5,7 +5,7 @@ function PortalShell({ children }: { children: React.ReactNode }) {
   const user = getSessionUser();
 
   return (
-    <div className="flex min-h-[calc(100vh-72px)] w-full bg-slate-50">
+    <div className="min-h-screen w-full bg-slate-50">
       <EnterpriseSidebar
         user={
           user
@@ -20,8 +20,10 @@ function PortalShell({ children }: { children: React.ReactNode }) {
             : null
         }
       />
-      <section className="min-w-0 flex-1 px-4 py-5 sm:px-6 lg:px-6 xl:px-8">
-        {children}
+      <section className="min-w-0 pl-[72px] pt-[64px] transition-all duration-300">
+        <div className="min-h-[calc(100vh-64px)] px-4 py-5 sm:px-6 lg:px-7 xl:px-8">
+          {children}
+        </div>
       </section>
     </div>
   );
